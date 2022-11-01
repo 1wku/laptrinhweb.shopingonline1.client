c5bbeb392b426b2556280342f584165c5ea10c0d
andreatran2002 <andreatran2002@gmail.com>
2022-11-01 11:11:51 +0700

    add default layout

---
+++ components/Home/Banner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrap">
+    wrapper
+    <h1 class="header">Helloe</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeBanner',
+}
+</script>
+
+<style></style>
