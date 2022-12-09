0897d4d26f985da19056b3dd4cbd2c30f3397c52
Phuong Vy <phuongvy.maicogroup@gmail.com>
2022-12-09 23:28:17 +0700

    update

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,18 @@
   <div>
     <home-banner />
     <HomeCategories />
-    <ProductsList />
+    <ProductsList :products="products" />
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  async asyncData({ params, $axios, $apiContext }) {
+    const slug = await params.slug // When calling /abc the slug will be "abc"
+    const res = await $axios($apiContext(`product/new/6`))
+
+    return { slug, products: res.data }
+  },
 }
 </script>
